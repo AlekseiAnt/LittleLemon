b1600ca3fa20f3901ecbf94cfc5a64c29881448a
Aleksei Antoniukov <AlekseiAnt@users.noreply.github.com>
2024-01-05 17:08:37 -0700

    connected to mysql and created models

--- littlelemonCap/restaurant/models.py
+++ littlelemonCap/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.DecimalField(primary_key=True, max_digits=11, decimal_places=0)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.DecimalField(max_digits=6, decimal_places=0)
+    bookingDate = models.DateTimeField()
+
+class Menu(models.Model):
+    id = models.DecimalField(primary_key=True, max_digits=11, decimal_places=0)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.DecimalField(max_digits=5, decimal_places=0)
