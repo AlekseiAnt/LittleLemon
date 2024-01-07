e422aa9b9ee3e3011a6a270343fae7a7041d0b98
Aleksei Antoniukov <AlekseiAnt@users.noreply.github.com>
2024-01-06 22:02:38 -0700

    added auth

--- littlelemonCap/restaurant/urls.py
+++ littlelemonCap/restaurant/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('booking/', views.BookingViewSet.as_view({'get': 'list'}))
+    path('booking/', views.BookingViewSet.as_view({'get': 'list'})),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token)
 ]
