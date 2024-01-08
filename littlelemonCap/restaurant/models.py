d8ecf3d9988102a4a046b4fe17a16b3d74fc82c9
Aleksei Antoniukov <AlekseiAnt@users.noreply.github.com>
2024-01-07 20:48:51 -0700

    final

--- littlelemonCap/restaurant/models.py
+++ littlelemonCap/restaurant/models.py
@@ -12,3 +12,5 @@ class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.DecimalField(max_digits=5, decimal_places=0)
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
